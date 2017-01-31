e6036f5362fde2c83125b2f37426d204bb476822
Alfonso G Austin-Rivera <aga23@afsconnect1.njit.edu>
2017-01-31 15:06:17 -0500

    Updated stuff

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
-<?php 
-	phpinfo();
+<?php
+
+		$obj = new main;
+
+		class main {
+
+			public function __construct(){
+					echo "program started</br>";
+			}
+
+			public function __destruct(){
+				echo 'program ended </br>';
+			}
+		}
+
+		class car {
+			
+		}
 
 ?>
