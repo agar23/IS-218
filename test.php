66ab697821ce546fcbb02137dac00d0f9cd503fb
Alfonso G Austin-Rivera <aga23@afsconnect1.njit.edu>
2017-01-31 15:37:03 -0500

    updated car class

--- test.php
+++ test.php
@@ -6,15 +6,46 @@
 
 			public function __construct(){
 					echo "program started</br>";
+
+					$car = carFactory::create();
+					$car->accel();
+					$car->decel();
+
+					print_r($car);
 			}
 
 			public function __destruct(){
-				echo 'program ended </br>';
+				echo '<br>program ended </br>';
 			}
 		}
 
+		class carFactory{
+				public static function create(){
+					$car = new car;
+					return $car;
+				}
+		}
+
 		class car {
-			
+
+				public $make;
+				public $model;
+				public $year;
+
+				public function __construct(){
+					$this->make = 'ford';
+					$this->year = date("y");
+
+				}
+				public function accel(){
+					echo "car is going faster</br>";
+				}
+				public function decel(){
+					echo "car is going slower</br>";
+
+				}
+
+
 		}
 
 ?>
