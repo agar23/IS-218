704ab5685b95c47347bc3c932ce3731f1d4ffb65
Alfonso G Austin-Rivera <aga23@afsconnect1.njit.edu>
2017-02-28 15:41:22 -0500

    Class end

--- ch05_ex1/model/category_db.php
+++ ch05_ex1/model/category_db.php
@@ -1,22 +1,22 @@
 <?php
 function get_categories() {
     global $db;
-    $query = 'SELECT * FROM categories
+    $query = 'SELECT * FROM categories_guitar1
               ORDER BY categoryID';
     $statement = $db->prepare($query);
     $statement->execute();
-    return $statement;    
+    return $statement;
 }
 
 function get_category_name($category_id) {
     global $db;
-    $query = 'SELECT * FROM categories
-              WHERE categoryID = :category_id';    
+    $query = 'SELECT * FROM categories_guitar1
+              WHERE categoryID = :category_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
-    $statement->execute();    
+    $statement->execute();
     $category = $statement->fetch();
-    $statement->closeCursor();    
+    $statement->closeCursor();
     $category_name = $category['categoryName'];
     return $category_name;
 }
