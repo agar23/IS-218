ca2240c8e80975dae49011ff9214d8f109634ee0
Alfonso G Austin-Rivera <aga23@afsconnect1.njit.edu>
2017-02-28 15:11:23 -0500

    Start HW5

---
+++ ch05_ex1/model/database.php
@@ -0,0 +1,14 @@
+<?php
+  $db_host  = 'sql2.njit.edu';
+  $db_name  = 'aga23';
+  $username = 'aga23';
+  $password = 'RaEYrCvE';
+
+    try {
+        $db = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
