6c3ead6d8f1c519a5468be7fd425e4fde3f8aed4
Alfonso G Austin-Rivera <aga23@afsconnect1.njit.edu>
2017-03-07 14:33:59 -0500

    fixed header issue

--- ch05_ex1/index.php
+++ ch05_ex1/index.php
@@ -1,4 +1,15 @@
-<?php include 'view/header.php'; ?>
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Alfonso's Shop</h1></header>
 <main>
     <h1>Menu</h1>
     <ul>
--- ch05_ex1/product_catalog/product_view.php
+++ ch05_ex1/product_catalog/product_view.php
@@ -2,19 +2,7 @@
 <main>
     <aside>
         <h1>Categories</h1>
-        <nav>
-            <ul class="nav">
-                <!-- display links for all categories -->
-                <?php foreach($categories as $category) : ?>
-                <li>
-                    <a href="?category_id=<?php 
-                              echo $category['categoryID']; ?>">
-                        <?php echo $category['categoryName']; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </nav>
+        <?php include '../view/categories_nav.php'; ?>
     </aside>
     <section>
         <h1><?php echo $name; ?></h1>
@@ -35,7 +23,7 @@
                 <input type="hidden" name="product_id"
                        value="<?php echo $product_id; ?>">
                 <b>Quantity:</b>
-                <input id="quantity" type="text" name="quantity" 
+                <input id="quantity" type="text" name="quantity"
                        value="1" size="2">
                 <br><br>
                 <input type="submit" value="Add to Cart">
