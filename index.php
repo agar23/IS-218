89f62afd59bdf799aa6bbb424ff42cf0bab6dcd3
agar23 <aga23@njit.edu>
2017-03-24 15:42:50 -0400

    updated index page

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php 
-    phpinfo();
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="main.css">
+</head>
 
-?>
+<!-- the body section -->
+<body>
+<header><h1>Alfonso's Shop</h1></header>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security 
